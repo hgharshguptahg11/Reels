034754272bec3d9e04ffa7c88641f35f030b2d86
unknown <hgharshguptahg11@gmail.com>
2022-02-24 17:36:08 +0530

    part-5

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -5,9 +5,33 @@ import Video from './Video'
 import './Posts.css'
 import Avatar from '@mui/material/Avatar';
 import Like from './Like';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea, CardActions } from '@mui/material';
+import Like2 from './Like2'
+import Addcomment from './Addcomment';
+import Comments from './Comments';
 
 function Posts({userData}) {
     const [posts, setPosts] = useState(null);
+    const [open, setOpen] = useState(null);
+
+    const handleClickOpen = (id) => {
+      setOpen(id);
+    };
+  
+    const handleClose = () => {
+      setOpen(null);
+    };
     useEffect(() => {
         let parr = []
         const unsub = database.posts.orderBy('createdAt', 'desc').onSnapshot((querySnapshot)=>{
@@ -35,6 +59,36 @@ function Posts({userData}) {
                                         <h4>{userData.fullname}</h4>
                                 </div>
                                 <Like userData = {userData} postData = {post}/>
+                                <ChatBubbleIcon className='chat-styling' onClick={()=>handleClickOpen(post.pId)}/>
+                                <Dialog
+                                    open={open==post.pId}
+                                    onClose={handleClose}
+                                    aria-labelledby="alert-dialog-title"
+                                    aria-describedby="alert-dialog-description"
+                                    fullWidth = {true}
+                                    maxWidth = 'md'
+
+                                >
+                                    <div className='modal-container'>
+                                        <div className='video-modal'>
+                                        <video autoPlay={true} muted="muted" controls >
+                                                    <source src={post.pUrl} />
+                                        </video>
+                                        </div>
+                                        <div className='comment-modal'>
+                                            <Card className='card1' style={{padding:'1rem'}}>
+                                                        <Comments postData={post}/>
+                                            </Card>
+                                            <Card variant="outlined" className="card2">
+                                                    <Typography style={{padding: '0.4rem'}}>{post.likes.length == 0 ? '' : `Liked by ${post.likes.length} users`}</Typography>
+                                                    <div style={{display: 'flex'}}>
+                                                        <Like2 postData={post} userData={userData} style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}} />
+                                                        <Addcomment userData={userData} postData={post}  />
+                                                    </div>
+                                                </Card>
+                                        </div>
+                                    </div>
+                                </Dialog>
                             </div>
                         </React.Fragment>
                     ))
